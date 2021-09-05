5be3a6ec9caed2f9f0e5d15518fbfe880d27962b
linhnguyen-js66 <linhnguyenchi227@gmail.com>
2021-09-06 04:16:53 +0700

    webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,65 @@
+const path = require("path")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 let mode = "development"
 let target = "web"
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
   mode = "production"
   tart = "browserslist"
 }
 module.exports = {
-    mode: mode,
-    target:target,
-    output:{
-      assetModuleFilename:"images/[hash][ext][query]"
-    },
-    module:{
-      rules:[
-        {
-            test:/\.(png|jpe?g|gif|svg)$/i,
-            type:'asset',
-            // parser:{
-            //   dataUrlCondition:{
-            //     maxSize:30*1024
-            //   }
-            // }
-        },
-        {
-          test:/\.(s[ac]|c)ss$/i,//compile => scss sass css
-          use:[
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options:{publicPath:""}
-            }
-            ,
-            "css-loader",
-            "postcss-loader",
-            "sass-loader"
-          ]
-        },
-        {
-          test:/\.jsx?$/,
-          exclude:/node_modules/,
-          use:{
-            loader:"babel-loader"
+  mode: mode,
+  target: target,
+  output: {
+    path:path.resolve(__dirname,"dist"),
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset',
+        // parser:{
+        //   dataUrlCondition:{
+        //     maxSize:30*1024
+        //   }
+        // }
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,//compile => scss sass css
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" }
           }
+          ,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    },
-    plugins: [new MiniCssExtractPlugin()],
-    resolve:{
-      extensions:[".js",".jsx"]
-    },
-    devtool: "source-map",
-    devServer: {
-      static: "./dist",
-      hot:true
-    },
-  };
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+    hot: true
+  },
+};
