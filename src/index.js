d52d6d24f1a8218dfedcc5d64c66b5153755ed59
linhnguyen-js66 <linhnguyenchi227@gmail.com>
2021-09-05 16:29:43 +0700

    new commit 5/9

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ const eleventShieldRecipe = {
     leatherStrip:2,
     ironIngot:1
 }
+const elevenGauntletsRecipe = { 
+    ...eleventShieldRecipe,
+    leather: 1,
+    refindedMoonstone:4
+}
+console.log(elevenGauntletsRecipe)
 console.log(eleventShieldRecipe)
